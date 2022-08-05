ce0ee4db871bf37b9ea9b5ed0ecc0768a73ec16c
Neo <91592026+AlexRixten@users.noreply.github.com>
2022-08-05 22:50:53 +0300

    create project

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { Button, Input, Flex, Checkbox, Heading } from "@chakra-ui/react";
+import { useStore } from "effector-react";
+
+import $store, {update, toggle, remove} from '../store'
+
+function TodoListItems() {
+
+  const store = useStore($store)
+
+  return (
+    <>
+      {store.todos.map((todo) => (
+        <Flex pt={2} key={todo.id}>
+          <Checkbox checked={todo.done} onClick={() => toggle(todo.id)} />
+          <Input mx={2} value={todo.text} onChange={(event) => update({id:todo.id, text:event.target.value})}/>
+          <Button onClick={() => remove(todo.id)}>Delete</Button>
+        </Flex>
+      ))}
+    </>
+  );
+}
+
+function TodoList() {
+  return (
+    <>
+      <Heading>Todo List</Heading>
+      <TodoListItems />
+    </>
+  );
+}
+
+export default TodoList;
